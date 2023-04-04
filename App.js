06747c06ff9ad180a38b7055528b309a1c80402d
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-04 12:18:46 +0300

    implement platform specific text styles

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
+import { View } from "react-native";
+import AppText from "./app/components/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppText>I love React Native </AppText>
+    </View>
+  );
 }
